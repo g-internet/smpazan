3b313823dff65e8eb5f78ea940c9dacb7bab4e25
g-internet <104988199+g-internet@users.noreply.github.com>
2022-06-20 12:18:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>daftar azan</title>
+</head>
+
+<body>
+    <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeK8wubsDTZ3jIJfHDiQu17x-alag66K7MciP8iEGRYLqROTg/viewform?usp=pp_url"
+        frameborder="2" width="1330" height="600"></iframe>
+</body>
+
+</html>
